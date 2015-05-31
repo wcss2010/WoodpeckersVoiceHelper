c31a161ba1b4c276cc3af7706d5e23cb2820730c
wcss <wcss2020@gmail.com>
2015-06-01 04:48:12 +0800

    提交基础工程。。。。。。

--- src/org/guihuotv/voice/VoiceCmdHandle.java
+++ src/org/guihuotv/voice/VoiceCmdHandle.java
@@ -1,5 +1,6 @@
 package org.guihuotv.voice;
 
+import java.util.Date;
 import java.util.List;
 
 import org.guihuotv.speech.helper.IatHelper;
@@ -9,6 +10,8 @@ import org.guihuotv.webservice.NgnSay;
 import android.webkit.WebView;
 
 public class VoiceCmdHandle {
+	static java.text.SimpleDateFormat times = new java.text.SimpleDateFormat("HH点mm分ss秒");
+	static java.text.SimpleDateFormat dates = new java.text.SimpleDateFormat("yyyy年MM月dd日");
 
 	/**
 	 * 处理语音指令
@@ -23,6 +26,12 @@ public class VoiceCmdHandle {
 			if (cmds.get(0).getSyscmd().equals("say")) {
 				// 只是回答问题
 				sayTxt = cmds.get(0).getCmdcontent();
+			} else if (cmds.get(0).getSyscmd().equals("saydate")) {
+				// 只是回答问题
+				sayTxt = "现在是" + dates.format(new Date());
+			} else if (cmds.get(0).getSyscmd().equals("saytime")) {
+				// 只是回答问题
+				sayTxt = "现在是" + times.format(new Date());
 			}
 		}
 
